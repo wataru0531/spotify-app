66e562441fbd9bc8fe627eec85bc990cccf5eaee
wataru0531 <obito0531@gmail.com>
2025-12-24 20:42:15 +0900

    form追加。Enterキーで発火できるように設定

--- src/components/SearchInput.jsx
+++ src/components/SearchInput.jsx
@@ -8,19 +8,34 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 export function SearchInput({ handleInputChange, getSearchSongs }) {
   // console.log(getSearchSongs)
 
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    getSearchSongs(); // 検索時にのみ引数なしで発火
+  }
+
   return (
     <section className="mb-10">
-      <input
-        className="bg-gray-700 w-1/3 p-2 rounded-l-lg focus:outline-none"
-        placeholder="探したい曲を入力してください"
-        onChange={ (e) => handleInputChange(e) }
-      />
-      <button 
-        className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-r-lg"
-        onClick={() => getSearchSongs()}
+      <form
+        className=""
+        // action="" // どのURLにデータを送信するかを指定
+                     // 書く必要なし。ページ遷移は不要 
+        onSubmit={ handleSubmit }
       >
-        <FontAwesomeIcon icon={faSearch} />
-      </button>
+        <input
+          className="bg-gray-700 w-1/3 p-2 rounded-l-lg focus:outline-none"
+          placeholder="探したい曲を入力してください"
+          onChange={ (e) => handleInputChange(e) }
+        />
+        <button
+          type="submit"
+          className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-r-lg"
+          // onClick={() => getSearchSongs()}
+        >
+          <FontAwesomeIcon icon={faSearch} />
+        </button>
+      </form>
+      
     </section>
   );
 }
