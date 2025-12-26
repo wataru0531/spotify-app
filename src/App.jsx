911366154810faa29f5785189c2ecab134d70b79
wataru0531 <obito0531@gmail.com>
2025-12-26 23:17:44 +0900

    クラスの修正

--- src/App.jsx
+++ src/App.jsx
@@ -3,16 +3,17 @@
 
 // TODO ... 検索結果の 20 / 総合件数を表示
 
-
 import { useEffect, useState } from "react";
 
-import spotify from "./lib/spotify";
+import SpotifyClient from "./lib/spotify";
 import { SongList } from "./components/SongList";
 import { SearchInput } from "./components/SearchInput.jsx";
 import { Pagination } from "./components/Pagination.jsx";
 
 
 export default function App() {
+  const [ spotify, setSpotify ] = useState(null); // SpotifyClientのインスタンス
+
   // Reactの大原則：状態は使う一番上の親コンポーネントに置く
   // const [ isLoading, setIsLoading ] = useState(true);
   const [ isLoading, setIsLoading ] = useState({
@@ -33,18 +34,19 @@ export default function App() {
     hasNext: false,
     hasPrev: false,
   });
-  
+
   const [ errorMessage, setErrorMessage ] = useState("");
 
   const limit = 20; // 1ページあたり20件データを取得
 
   // ✅ 最近流行りの曲を取得
-  const fetchPopularSongs = async () => {
+  const fetchPopularSongs = async (_client) => {
+    // console.log(_client);
     try {
       // setIsLoading(true);
       setIsLoading(prev => ({ ...prev, popular: true }))
 
-      const result = await spotify.getPopularSongs();
+      const result = await _client.getPopularSongs();
       // console.log(result.items); // (98) [{…}, {…}, {…}, {…}, ...]
 
       // ✅ 曲情報のみの配列として取得
@@ -71,6 +73,8 @@ export default function App() {
 
   // ✅ 検索したい音楽を取得する
   const getSearchSongs = async (_page = 1) => {
+    if(!spotify) return; // 
+
     // console.log(keyword)
     if(keyword.trim() === ""){
       // console.worn("keyword is empty");
@@ -111,27 +115,27 @@ export default function App() {
 
   // ✅ 次へボタンクリック
   const moveToNext = async () => {
-    // const nextPage = page + 1;
-
-    // await getSearchSongs(nextPage)
-    // setPage(nextPage);
-
     if(!pagination.hasNext) return;
     await getSearchSongs(pagination.page + 1);
   }
 
   // ✅ 前へ戻るボタンクリック
   const moveToPrev = async () => {
-    // const prevPage = page -1;
-    // await getSearchSongs(prevPage);
-    // setPage(prevPage);
     if(!pagination.hasPrev) return;
     await getSearchSongs(pagination.page - 1);
   }
 
-
+  // ✅ コンポーネントに描画後に発火
   useEffect(() => {
-    fetchPopularSongs();
+    const init = async () => {
+      // ⭐️ SpotifyClient 初期化
+      const client = await SpotifyClient.initialize();
+      // console.log(client)
+      setSpotify(client);
+
+      await fetchPopularSongs(client);
+    };
+    init();
   }, []);
   
   return (
@@ -182,3 +186,191 @@ export default function App() {
     </div>
   );
 }
+
+
+
+
+
+
+
+// import { useEffect, useState } from "react";
+
+// import SpotifyClient from "./lib/spotify";
+// import { SongList } from "./components/SongList";
+// import { SearchInput } from "./components/SearchInput.jsx";
+// import { Pagination } from "./components/Pagination.jsx";
+
+
+// export default function App() {
+//   // ⭐️ SpotifyClient 初期化
+//   const spotify = await SpotifyClient.initialize();
+
+//   // Reactの大原則：状態は使う一番上の親コンポーネントに置く
+//   // const [ isLoading, setIsLoading ] = useState(true);
+//   const [ isLoading, setIsLoading ] = useState({
+//     popular: true, // 人気曲検索用のローディング状況
+//     search: false, // 検索用のローディング状況
+//   });
+
+//   const [ popularSongs, setPopularSongs ] = useState([]);
+//   const [ keyword, setKeyword ] = useState("");
+//   const [ searchedSongs, setSearchedSongs ] = useState(null);
+//   const isSearchedResult = searchedSongs != null; // 検索文字列があればtrue
+//                                                   // → handleInputなどが発火されると、コンポーネント自体が再レンダリング
+//                                                   //   されるから動的にここも変更される。
+//                                                   //   再レンダリングは、ステートが変更した時だけ起きる
+
+//   const [ pagination, setPagination ] = useState({
+//     page: 1,
+//     hasNext: false,
+//     hasPrev: false,
+//   });
+
+//   const [ errorMessage, setErrorMessage ] = useState("");
+
+//   const limit = 20; // 1ページあたり20件データを取得
+
+//   // ✅ 最近流行りの曲を取得
+//   const fetchPopularSongs = async () => {
+//     try {
+//       // setIsLoading(true);
+//       setIsLoading(prev => ({ ...prev, popular: true }))
+
+//       const result = await spotify.getPopularSongs();
+//       // console.log(result.items); // (98) [{…}, {…}, {…}, {…}, ...]
+
+//       // ✅ 曲情報のみの配列として取得
+//       const popularSongsTrack = result.items.map(item => {
+//         return item.track; // 👉 trackに音楽の情報が格納されている
+//       });
+//       // console.log(popularSongsTrack)
+
+//       setPopularSongs(popularSongsTrack);
+//     } catch(e){
+//       console.error("Failed to fetch popular songs", e);
+//     } finally {
+//       // console.log("finally")
+//       // setIsLoading(false);
+//       setIsLoading(prev => ({ ...prev, popular: false }))
+//     }
+//   }
+
+//   // ✅ 検索文字列を取得
+//   const handleInputChange = (e) => {
+//     // console.log(e.target.value);
+//     setKeyword(e.target.value);
+//   }
+
+//   // ✅ 検索したい音楽を取得する
+//   const getSearchSongs = async (_page = 1) => {
+//     // console.log(keyword)
+//     if(keyword.trim() === ""){
+//       // console.worn("keyword is empty");
+//       setErrorMessage("検索キーワードを入力してください。");
+//       return;
+//     }
+//     setErrorMessage("");
+    
+//     try {
+//       // setIsLoading(true);
+//       setIsLoading(prev => ({ ...prev, search: true }));
+
+//       // ✅ offset ... 何件目からデータを取るか。
+//       // 👉 欲しいページに対する、曲の最初の件数を取得。引数がからなら0を返す
+//       // parseInt() ... 数字に変換
+//       const offset = parseInt(_page) ? (parseInt(_page) - 1) * limit : 0;
+//       const result = await spotify.searchSongs(keyword, limit, offset);
+//       // console.log(result); // { href: 'https://api.spotify.com/v1/search?offset=0&limit=2…uki&type=track&locale=ja,en-US;q%3D0.9,en;q%3D0.8', limit: 20, next: 'https://api.spotify.com/v1/search?offset=20&limit=…uki&type=track&locale=ja,en-US;q%3D0.9,en;q%3D0.8',... }
+
+//       // console.log(!result.next); // false。真偽地にして反転
+//       // console.log(!!result.next); // true。元に戻す
+//       setPagination({
+//         page: _page,
+//         hasNext: !!result.next, // next ... 次のページを取得するための完全なURL
+//         hasPrev: !!result.previous, // nextと同様
+//       })
+
+//       setSearchedSongs(result.items || []);
+//     } catch(e) {
+//       console.error("Failed to search songs", e);
+//       setErrorMessage("検査に失敗しました。もう一度お試しください。");
+//       setSearchedSongs([])
+//     } finally {
+//       // setIsLoading(false);
+//       setIsLoading(prev => ({ ...prev, search: false }));
+//     }
+//   }
+
+//   // ✅ 次へボタンクリック
+//   const moveToNext = async () => {
+//     // const nextPage = page + 1;
+
+//     // await getSearchSongs(nextPage)
+//     // setPage(nextPage);
+
+//     if(!pagination.hasNext) return;
+//     await getSearchSongs(pagination.page + 1);
+//   }
+
+//   // ✅ 前へ戻るボタンクリック
+//   const moveToPrev = async () => {
+//     // const prevPage = page -1;
+//     // await getSearchSongs(prevPage);
+//     // setPage(prevPage);
+//     if(!pagination.hasPrev) return;
+//     await getSearchSongs(pagination.page - 1);
+//   }
+
+
+//   useEffect(() => {
+//     fetchPopularSongs();
+//   }, []);
+  
+//   return (
+//     <div className="flex flex-col min-h-screen bg-gray-900 text-white">
+//       {
+//         isLoading.popular ?
+//           <div className="h-screen flex items-center justify-center">
+//             <p>Loading...</p>
+//           </div>
+//         : (
+//           <main className="flex-1 p-8 mb-20">
+//             <header className="flex justify-between items-center mb-10">
+//               <h1 className="text-4xl font-bold">Music App</h1>
+//             </header>
+
+//             <SearchInput 
+//               keyword={ keyword }
+//               handleInputChange={ handleInputChange } 
+//               getSearchSongs={ getSearchSongs }
+//               errorMessage={ errorMessage }
+//             />
+
+//             <section>
+//               <h2 className="text-2xl font-semibold mb-5">
+//                 { isSearchedResult ? "SearchResult" : "Popular Songs"}
+//               </h2>
+
+//               <SongList
+//                 isLoading={ isSearchedResult ? isLoading.search : isLoading.popular }
+//                 // デフォルト → 流行りの曲一覧。検索した場合は検索結果を一覧で表示
+//                 songs={ isSearchedResult ? searchedSongs : popularSongs } 
+//               />
+
+//               {/* ページネーション */}
+//               { 
+//                 isSearchedResult &&
+//                   <Pagination 
+//                     moveToPrev={ pagination.hasPrev ? moveToPrev : null }
+//                     moveToNext={ pagination.hasNext ? moveToNext : null } 
+//                   /> 
+//               }
+
+//             </section>
+//           </main>
+//         )
+//       }
+      
+//     </div>
+//   );
+// }
--- src/lib/spotify.js
+++ src/lib/spotify.js
@@ -3,85 +3,241 @@
 import axios from "axios";
 
 
+const SPOTIFY_ACCOUNTS_URL = "https://accounts.spotify.com/api";
+const SPOTIFY_API_URL = "https://api.spotify.com/v1";
+const POPULAR_PLAYLIST_ID = "5SLPaOxQyJ8Ne9zpmTOvSe";
+
+
 class SpotifyClient {
-  // ✅ 初期化 → アクセストークン付きのインスタンスが返る
-  static async initialize(){
-    const response = await axios.post(
-      "https://accounts.spotify.com/api/token",
-      {
-        grant_type: "client_credentials", // どの認証方法を使いますか？
-                                          // → ユーザーのログインを使わず、アプリIDと秘密鍵だけで認証する方式
-                                          // client_credentials → アプリ自身としてアクセス
-                                          //                      ・トークンは1つ
-                                          //                      ・全ユーザーで共有
-                                          //                      ・公開情報のみ
-        client_id: import.meta.env.VITE_SPOTIFY_CLIENT_ID, // Spotify開発者ページで発行されるアプリID
-        client_secret: import.meta.env.VITE_SPOTIFY_CLIENT_SECRET, // アプリ専用の秘密鍵
+  constructor({ accessToken, expiresAt }) {
+    this.accessToken = accessToken;
+    this.expiresAt = expiresAt;
+
+    this.api = axios.create({
+      baseURL: SPOTIFY_API_URL,
+      headers: {
+        Authorization: `Bearer ${this.accessToken}`,
       },
-      {
-        headers: {
-          "Content-Type": "application/x-www-form-urlencoded", // 送信データはフォーム形式で送れということ
-                                                               // form-urlencoded 
-                                                               // → フォームの値をURL形式にエンコードする
-                                                               //   HTMLのフォーム送信と同じ形式で、
-                                                               //   データを「キー=値&キー=値」の形にエンコードして送ります
+    });
+  }
+
+  // ✅ スタティック
+  static async initialize(){
+    const { accessToken, expiresAt } = await this.#fetchAccessToken();
+    return new SpotifyClient({ accessToken, expiresAt })
+  }
+
+  // ✅ 初期化 → アクセストークン付きのインスタンスを返す
+  // ⭐️ # → private(プライベート)メンバーを表す記号。
+  //        functionを省略可能。クラス内では省略できる。
+  static async #fetchAccessToken(){
+    try{
+      const response = await axios.post(
+        `${SPOTIFY_ACCOUNTS_URL}/token`,
+        new URLSearchParams({ // ⭐️ TODO
+          grant_type: "client_credentials", // どの認証方法を使いますか？
+                                            // → ユーザーのログインを使わず、アプリIDと秘密鍵だけで認証する方式
+                                            // client_credentials → アプリ自身としてアクセス
+                                            //                      ・トークンは1つ
+                                            //                      ・全ユーザーで共有
+                                            //                      ・公開情報のみ
+          client_id: import.meta.env.VITE_SPOTIFY_CLIENT_ID, // Spotify開発者ページで発行されるアプリID
+          client_secret: import.meta.env.VITE_SPOTIFY_CLIENT_SECRET, // アプリ専用の秘密鍵
+        }),
+        {
+          headers: {
+            "Content-Type": "application/x-www-form-urlencoded", // 送信データはフォーム形式で送れということ
+                                                                // form-urlencoded 
+                                                                // → フォームの値をURL形式にエンコードする
+                                                                //   HTMLのフォーム送信と同じ形式で、
+                                                                //   データを「キー=値&キー=値」の形にエンコードして送ります
+          }
         }
+      );
+      // console.log(response); // { data: {…}, status: 200, statusText: '', headers: AxiosHeaders, config: {…}, …}
+
+      const { access_token, expires_in } = response.data;
+      // console.log(expires_in); // 3600 → アクセストークンが何秒間有効か。1時間だけ有効
+
+      // ⭐️ここから⭐️ここから⭐️ここから⭐️ここから⭐️ここから⭐️ここから⭐️ここから⭐️ここから
+      // ⭐️ここから⭐️ここから⭐️ここから⭐️ここから⭐️ここから⭐️ここから⭐️ここから⭐️ここから
+      // console.log(Date.now())
+      return {
+        accessToken: access_token,
+        expiresAt: Date.now() + expires_in * 1000, // 👉 絶対時刻に変換。
+                                                   //    * 1000 ... ミリ秒に変換。Date.now()に揃えるため
       }
-    );
-    // console.log(response); // { data: {…}, status: 200, statusText: '', headers: AxiosHeaders, config: {…}, …}
+    } catch(e){
+      throw new Error("Spotify認証に失敗しました。", e);
+    }
+  }
 
-    let spotify = new SpotifyClient(); // ✅ インスタンス化
-    spotify.token = response.data.access_token;
+  // ✅ トークンの更新
+  async #refreshTokenIfNeeded() {
+    if (Date.now() < this.expiresAt) return;
 
-    return spotify;
+    const { accessToken, expiresAt } = await SpotifyClient.#fetchAccessToken();
+
+    this.accessToken = accessToken;
+    this.expiresAt = expiresAt;
+    this.api.defaults.headers.Authorization = `Bearer ${accessToken}`;
   }
 
   // ✅ 最近流行りの曲を取得
-  async getPopularSongs(){
+  async getPopularSongs(_playlistId = POPULAR_PLAYLIST_ID){
     // console.log(this.token);
+    if(!_playlistId) throw new Error("_playlistId が指定されていません。");
 
-    const response = await axios.get(
-      "https://api.spotify.com/v1/playlists/5SLPaOxQyJ8Ne9zpmTOvSe",
-      {
-        headers: {
-          Authorization: `Bearer ${this.token}` // Bearer → このトークンを持っている者が正当な利用者」という意味の認証方式名。
-        }
-      }
-    );
-    // console.log(response.data);
-    return response.data.tracks;
+    await this.#refreshTokenIfNeeded(); // トークンの更新
+
+    try {
+      // const response = await axios.get(
+      //   "https://api.spotify.com/v1/playlists/5SLPaOxQyJ8Ne9zpmTOvSe",
+      //   {
+      //     headers: {
+      //       Authorization: `Bearer ${this.token}` // Bearer → このトークンを持っている者が正当な利用者」という意味の認証方式名。
+      //     }
+      //   }
+      // );
+      // console.log(response.data);
+      // return response.data.tracks;
+
+      const response = await this.api.get(`/playlists/${_playlistId}`)
+      return response.data.tracks;
+    } catch(e){
+      console.log("getPopularSongs failed", e);
+      throw new Error("最近流行りの曲の取得に失敗しました；")
+    }
   }
 
   // ✅ 曲の検索
   async searchSongs(_keyword, _limit, _offset){ 
     // console.log(_offset);
+    if(!_keyword?.trim()) throw new Error("検索キーワードが空です。");
 
-    const response = await axios.get(
-      `https://api.spotify.com/v1/search`,
-      {
-        headers: {
-          Authorization: `Bearer ${this.token}`,
-        },
+    await this.#refreshTokenIfNeeded(); // トークンを更新
+
+    try{
+      // `https://api.spotify.com/v1/search`,
+      const response = await this.api.get("/search", {
         params: { // URLの ?key=value の部分をaxiosが自動で作ってくれる仕組み
                   // → ?q=曲名&type=track に変換されてSpotifyに送られる
           q: _keyword,
           type: "track", // 検索結果を曲だけに
           limit: _limit, // 件数制限
           offset: _offset, // ⭐️ 先頭から曲を何件スキップするか。
-                           // → Spotifyは、indexは0から並び、曲は1から始まる。
-                           // → 0なら、 0件スキップ → index 0〜19を返す
-                           // → 20なら、20件スキップ → index 20〜39 を返す
+                          // → Spotifyは、indexは0から並び、曲は1から始まる。
+                          // → 0なら、 0件スキップ → index 0〜19を返す
+                          // → 20なら、20件スキップ → index 20〜39 を返す
           // market: "JP", // 日本で再生可能な曲のみ取得
           // include_external: ,
         }
-      }
-    );
-    // console.log(response)
+      });
+      // console.log(response)
 
-    return response.data.tracks;
+      return response.data.tracks;
+    } catch(e){
+      console.error("searchSongs failed", e);
+      throw new Error("曲検索に失敗しました。");
+    }
   }
 }
 
-const spotify = await SpotifyClient.initialize();
+// const spotify = await SpotifyClient.initialize();
 
-export default spotify;
+export default SpotifyClient;
+
+
+
+
+
+
+
+
+
+
+
+// import axios from "axios";
+
+// class SpotifyClient {
+//   // ✅ 初期化 → アクセストークン付きのインスタンスを返す
+//   static async initialize(){
+//     const response = await axios.post(
+//       "https://accounts.spotify.com/api/token",
+//       {
+//         grant_type: "client_credentials", // どの認証方法を使いますか？
+//                                           // → ユーザーのログインを使わず、アプリIDと秘密鍵だけで認証する方式
+//                                           // client_credentials → アプリ自身としてアクセス
+//                                           //                      ・トークンは1つ
+//                                           //                      ・全ユーザーで共有
+//                                           //                      ・公開情報のみ
+//         client_id: import.meta.env.VITE_SPOTIFY_CLIENT_ID, // Spotify開発者ページで発行されるアプリID
+//         client_secret: import.meta.env.VITE_SPOTIFY_CLIENT_SECRET, // アプリ専用の秘密鍵
+//       },
+//       {
+//         headers: {
+//           "Content-Type": "application/x-www-form-urlencoded", // 送信データはフォーム形式で送れということ
+//                                                                // form-urlencoded 
+//                                                                // → フォームの値をURL形式にエンコードする
+//                                                                //   HTMLのフォーム送信と同じ形式で、
+//                                                                //   データを「キー=値&キー=値」の形にエンコードして送ります
+//         }
+//       }
+//     );
+//     // console.log(response); // { data: {…}, status: 200, statusText: '', headers: AxiosHeaders, config: {…}, …}
+
+//     let spotify = new SpotifyClient(); // ✅ インスタンス化
+//     spotify.token = response.data.access_token;
+
+//     return spotify;
+//   }
+
+//   // ✅ 最近流行りの曲を取得
+//   async getPopularSongs(){
+//     // console.log(this.token);
+
+//     const response = await axios.get(
+//       "https://api.spotify.com/v1/playlists/5SLPaOxQyJ8Ne9zpmTOvSe",
+//       {
+//         headers: {
+//           Authorization: `Bearer ${this.token}` // Bearer → このトークンを持っている者が正当な利用者」という意味の認証方式名。
+//         }
+//       }
+//     );
+//     // console.log(response.data);
+//     return response.data.tracks;
+//   }
+
+//   // ✅ 曲の検索
+//   async searchSongs(_keyword, _limit, _offset){ 
+//     // console.log(_offset);
+
+//     const response = await axios.get(
+//       `https://api.spotify.com/v1/search`,
+//       {
+//         headers: {
+//           Authorization: `Bearer ${this.token}`,
+//         },
+//         params: { // URLの ?key=value の部分をaxiosが自動で作ってくれる仕組み
+//                   // → ?q=曲名&type=track に変換されてSpotifyに送られる
+//           q: _keyword,
+//           type: "track", // 検索結果を曲だけに
+//           limit: _limit, // 件数制限
+//           offset: _offset, // ⭐️ 先頭から曲を何件スキップするか。
+//                            // → Spotifyは、indexは0から並び、曲は1から始まる。
+//                            // → 0なら、 0件スキップ → index 0〜19を返す
+//                            // → 20なら、20件スキップ → index 20〜39 を返す
+//           // market: "JP", // 日本で再生可能な曲のみ取得
+//           // include_external: ,
+//         }
+//       }
+//     );
+//     // console.log(response)
+
+//     return response.data.tracks;
+//   }
+// }
+
+// const spotify = await SpotifyClient.initialize();
+
+// export default spotify;
